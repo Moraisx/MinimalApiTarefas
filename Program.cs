6010660466d73981513e714ca003a5b51cc5ba7f
Tiago Morais <tiagomoraisx.silvax@gmail.com>
2022-12-28 10:20:14 -0300

    Inc metodos Get - Delete - Put

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 //Microsoft.EntityFrameworkCore.InMemory
+//Criar banco de dados em memória
 builder.Services.AddDbContext<AppDbContext>(options => 
 options.UseInMemoryDatabase("TarefaDB"));
 
@@ -23,10 +24,9 @@ if (app.Environment.IsDevelopment())
 }
 
 //app.MapGet("/", () => "Return");
-
 //Metodo Get que retorna frases aleatórias
-app.MapGet("frases_aleatorias", async () => 
-await new HttpClient().GetStringAsync("https://ron-swanson-quotes.herokuapp.com/v2/quotes"));
+//app.MapGet("frases_aleatorias", async () => 
+//await new HttpClient().GetStringAsync("https://ron-swanson-quotes.herokuapp.com/v2/quotes"));
 
 app.MapGet("/tarefas", async (AppDbContext db) =>
 {
@@ -50,8 +50,16 @@ app.MapGet("/tarefas/{id}", async (AppDbContext db, int id) =>
     //outra possibilidade de retorno
     //await db.Tarefas.FindAsync(id) is Tarefa tarefa ? Results.Ok(tarefa) : Results.NotFound());
 });
-   
 
+app.MapGet("/tarefas/concluida", async (AppDbContext db) =>
+{
+    return await db.Tarefas.Where(tarefa => tarefa.IsConcluida).ToListAsync();
+});
+
+app.MapGet("/tarefas/Nao-concluida", async (AppDbContext db) =>
+{
+    return  await db.Tarefas.Where(tarefa => tarefa.IsConcluida == false).ToListAsync();
+});
 
 app.MapPost("/tarefas", async (Tarefa tarefa, AppDbContext db) =>
 {
@@ -64,6 +72,37 @@ app.MapPost("/tarefas", async (Tarefa tarefa, AppDbContext db) =>
     return Results.Created($"/tarefa/{tarefa.Id}", tarefa);
 });
 
+app.MapPut("/tarefa/{id}", async (Tarefa tarefa, AppDbContext db, int id) =>
+{
+    var tarefaLocalizada = await db.Tarefas.FindAsync(id);
+
+    if(tarefaLocalizada != null)
+    {
+        tarefaLocalizada.Nome = tarefa.Nome;
+        tarefaLocalizada.IsConcluida = tarefa.IsConcluida;
+
+        await db.SaveChangesAsync();
+
+        return Results.Ok(tarefaLocalizada);
+    }
+    else
+    {
+        return Results.NotFound($"Tarefa {id} não encontrada");
+    }
+});
+
+app.MapDelete("/tarefa/{id}", async (int id, AppDbContext db) =>
+{
+    if(await db.Tarefas.FindAsync(id) is Tarefa tarefa)
+    {
+        db.Tarefas.Remove(tarefa);
+        await db.SaveChangesAsync();
+        return Results.Ok(tarefa);
+    }
+
+    return Results.NotFound($"Tarefa {id} não encontrada");
+});
+
 app.Run();
 
 class Tarefa
@@ -71,13 +110,11 @@ class Tarefa
     public int Id { get; set; }
     public string? Nome { get; set; }
     public bool IsConcluida { get; set; }
-
 }
 
 class AppDbContext: DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){}
-
     public DbSet<Tarefa> Tarefas => Set<Tarefa>();
 }   
 
